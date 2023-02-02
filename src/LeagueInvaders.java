b2c5df36a13054e5c3463c3382ab7dfa6497ad29
league <league@imac-14.attlocal.net>
2023-02-01 19:56:49 -0800

    worked on space invaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,19 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame gameFrame;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;		
+		public static void main(String[] args) {
+			LeagueInvaders game = new LeagueInvaders();
+			game.setup();
+		}
+		LeagueInvaders() {
+			this.gameFrame = new JFrame();;
+		}
+		void setup() {
+			gameFrame.setSize(WIDTH, HEIGHT);
+			gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			gameFrame.setVisible(true);
+		}
 }
