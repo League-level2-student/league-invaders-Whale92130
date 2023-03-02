f70faf3176c0c871b4a55f30896765f0a555ce23
league <league@imac-14.attlocal.net>
2023-03-01 19:55:03 -0800

    worked on legue invaders

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,8 +1,21 @@
+import java.util.ArrayList;
+import java.util.Random;
 
 public class ObjectManager {
 	Rocketship rocket;
-	
+	ArrayList projectile = new ArrayList();
+	ArrayList alien = new ArrayList();
+	Random random = new Random();
 	ObjectManager(Rocketship ship) {
-		rocket = new RocketShip();
+		rocket = ship;
+	}
+	void addProjectile(Projectile project) {
+		projectile.add(project);
+	}
+	void addAlien(Alien ali) {
+		alien.add(new Alien(random.nextInt(LeagueInvaders.WIDTH),0,50,50));
+	}
+	void update() {
+		
 	}
 }
